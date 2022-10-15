3b2e237d44cbe2a31bb9ed48fea5c7ffd0c3a4c7
talha <talha@gmail.com>
2022-10-15 13:01:37 +0300

    cards

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -7,9 +7,9 @@ import { data } from "../helpers/data"
 const Card = () => {
   console.log(data)
   return (
-    <Container className="rounded-4 mt-4" style={{ backgroundColor: "#f48b29" }}>
-      <h1>Language</h1>
-      <Row>
+    <Container className="rounded-4 mt-4 p-4" style={{ backgroundColor: "#f48b29" }}>
+      <h1 className="text-white my-2">Language</h1>
+      <Row className="g-3 justify-content-center">
         {data.map((lang, index) => {
           return (
             <Col sm={6} md={4} lg={3} key={index}>
--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-
-const Language = ({name , img, options}) => {
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+const Language = ({ name, img, options }) => {
   // console.log(lang)
-  return (  
-    <div>Language</div>
-  )
-}
+  return (
+    <Container style={{background:"peachpuff"}} className="p-4 rounded-2" >
+      <Container>
+        <Image src={img} width="60%"></Image>
+        <h3>{name}</h3>
+      </Container>
+    </Container>
+  );
+};
 
 export default Language
