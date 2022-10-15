af48b1626876fc04013644b161a729c4feb33cd0
talha <talha@gmail.com>
2022-10-15 14:01:17 +0300

    finish

--- src/components/Language.jsx
+++ src/components/Language.jsx
@@ -1,23 +1,28 @@
 import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
+
 const Language = ({ name, img, options }) => {
   // console.log(lang)
   const [showDesc, setShowDesc] = useState(false)
   console.log(showDesc)
   return (
-    <Container style={{ background: "peachpuff" }} className="p-4 rounded-2" onClick={() => setShowDesc(!showDesc)}>
-      <Container>
-        <Image src={img} width="60%"></Image>
-        <h3>{name}</h3>
-      </Container>
-      <ol>
-        {options.map((item)=>{
-          return(
-            <li className="h5 text-start">{item}</li>
-          )
-        })}
-      </ol>
+    <Container style={{ background: "peachpuff" }} className="p-4 rounded-2 h-100 lang-card" onClick={() => setShowDesc(!showDesc)} type="button">
+      {!showDesc &&
+        <Container>
+          <Image src={img} width="60%" className="lang-logo"></Image>
+          <h3>{name}</h3>
+        </Container>
+      }
+      {showDesc &&
+        <ol className="h-100 d-flex flex-column justify-content-center">
+          {options.map((item) => {
+            return (
+              <li className="h5 text-start">{item}</li>
+            )
+          })}
+        </ol>
+      }
     </Container>
   );
 };
